f430be7aed9d4d94c55768df57b5941373d60ed2
andrewgremlich <andrew.gremlich@gmail.com>
2022-03-28 10:21:49 -0600

    Initial Commit

---
+++ components/Text/index.tsx
@@ -0,0 +1,5 @@
+import { FC } from "react";
+
+export const H1: FC = ({ children }) => {
+  return <h1>{children}</h1>;
+};
---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { GetServerSideProps, NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+import { H1 } from "components/Text";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <H1>Hello world</H1>
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  console.log(process.env.USDA_API_KEY);
+  return { props: {} };
+};
+
+export default Home;
