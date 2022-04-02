b213b1ec698ee80290f73ad78fcc4aca964062a5
andrewgremlich <andrew.gremlich@gmail.com>
2022-04-02 11:29:10 -0600

    Added getFoodItem api

---
+++ pages/api/getFoodItem.ts
@@ -0,0 +1,22 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+};
+
+type QueryParams = {
+  foodId: string;
+};
+
+export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const queryParams = req.query as QueryParams;
+
+  const fetchUSDAdata = await fetch(
+    `https://api.nal.usda.gov/fdc/v1/food/${queryParams.foodId}?api_key=${process.env.USDA_API_KEY}`
+  );
+  const data = await fetchUSDAdata.json();
+
+  console.log(data);
+
+  res.status(200).json({ name: "John Doe" });
+};
