caa2dc0c48fe02b1edf30e2d6511ceb97f625394
andrewgremlich <andrew.gremlich@gmail.com>
2022-04-05 11:44:21 -0600

    Made the main page the feature and search Results a ui component

--- components/feature/FoodSearch/index.tsx
+++ components/feature/FoodSearch/index.tsx
@@ -1,46 +1,23 @@
-import { FC, useRef, useState } from "react";
-
-import { SearchResults } from "components/SearchResults";
+import { FC, MouseEventHandler, FormEventHandler } from "react";
 
 import styles from "./styles.module.css";
 
-export const FoodSearch: FC = () => {
-  const [searchResults, setSearchResults] = useState<any[]>([]);
-  const [foodQuery, setFoodQuery] = useState("");
-  const [pageSize, setPageSize] = useState(10);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  const getSearchResults = () => {
-    const queryParams = new URLSearchParams({
-      foodQuery,
-      pageSize,
-      pageNumber,
-    } as unknown as URLSearchParams);
-
-    fetch(`/api/foods/search?${queryParams}`)
-      .then((res) => res.json())
-      .then((res) => {
-        setSearchResults(res.foods)
-      });
-  };
-
-  return (
-    <div>
-      <div className={styles["search-bar"]}>
-        <input
-          className={styles["search-input"]}
-          type="search"
-          placeholder="Search Foods"
-          onChange={(e) => setFoodQuery(e.target.value)}
-        />
-        <input
-          onClick={getSearchResults}
-          className={styles["search-submit"]}
-          type="submit"
-          value="Search!"
-        />
-      </div>
-      <SearchResults />
-    </div>
-  );
+type FoodSearchProps = {
+  getSearchResults: FormEventHandler<HTMLFormElement>;
+  setFoodQuery: (query: string) => void;
 };
+
+export const FoodSearch: FC<FoodSearchProps> = ({
+  setFoodQuery,
+  getSearchResults,
+}) => (
+  <form onSubmit={getSearchResults} className={styles["search-bar"]}>
+    <input
+      className={styles["search-input"]}
+      type="search"
+      placeholder="Search Foods"
+      onChange={(evt) => setFoodQuery(evt.target.value)}
+    />
+    <input className={styles["search-submit"]} type="submit" value="Search!" />
+  </form>
+);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,42 @@
+import { useState, FormEventHandler } from "react";
 import type { GetServerSideProps, NextPage } from "next";
-import Head from "next/head";
 
 import { FoodSearch } from "components/FoodSearch";
+import { SearchResults } from "components/SearchResults";
+import { FoodItem } from "models/foods";
 
-const Home: NextPage = () => (
-  <div>
-    {/* Day Carousel with total for day */}
-    {/* Estimation of needed calories per day */}
-    <FoodSearch />
-  </div>
-);
+const Home: NextPage = () => {
+  const [searchResults, setSearchResults] = useState<FoodItem[]>([]);
+  const [foodQuery, setFoodQuery] = useState("");
+  const [pageSize, setPageSize] = useState(10);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const getSearchResults: FormEventHandler<HTMLFormElement> = (evt) => {
+    evt.preventDefault();
+
+    const queryParams = new URLSearchParams({
+      foodQuery,
+      pageSize,
+      pageNumber,
+    } as unknown as URLSearchParams);
+
+    fetch(`/api/foods/search?${queryParams}`)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setSearchResults(res.foods);
+      });
+  };
+
+  return (
+    <div>
+      {/* Day Carousel with total for day */}
+      {/* Estimation of needed calories per day */}
+      <FoodSearch {...{ getSearchResults, setFoodQuery }} />
+      <SearchResults />
+    </div>
+  );
+};
 
 export const getServerSideProps: GetServerSideProps = async () => {
   return { props: {} };
