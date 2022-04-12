3701f0cc73b3dd53c1176b8fba5c737784954029
Andrew Gremlich <andrew.gremlich@gmail.com>
2022-04-12 10:30:35 -0600

    Styled search results

--- components/SearchResults/index.tsx
+++ components/SearchResults/index.tsx
@@ -2,16 +2,31 @@ import { FC } from "react";
 
 import { FoodItem } from "models/foods";
 
+import styles from "./styles.module.css";
+
 type SearchResultsProps = {
   searchResults: FoodItem[];
+  addToKcal: (kcal: number) => void;
 };
 
-export const SearchResults: FC<SearchResultsProps> = ({ searchResults }) => (
-  <div>
-    {searchResults.map((food) => (
-      <div key={food.id}>
-        <p>{food.brandName}</p>
-      </div>
-    ))}
+export const SearchResults: FC<SearchResultsProps> = ({
+  searchResults,
+  addToKcal,
+}) => (
+  <div className={`${styles["search-results"]}`}>
+    {searchResults.map((food) => {
+      const kcal = food.foodNutrients.find(
+        (nutrient) => nutrient.unitName === "KCAL"
+      )?.value;
+
+      return (
+        <div key={food.fdcId} onClick={() => addToKcal(kcal ?? 0)}>
+          <p>{food.brandName}</p>
+          <p>{food.brandOwner}</p>
+          <p>{food.description}</p>
+          <p>{kcal} kcal</p>
+        </div>
+      );
+    })}
   </div>
 );
