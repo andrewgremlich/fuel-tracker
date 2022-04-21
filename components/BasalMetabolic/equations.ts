a4f0918a85d0d9e3c5ef5846ffb8f64b46a3af98
andrewgremlich <andrew.gremlich@gmail.com>
2022-04-21 10:58:47 -0600

    Changed to dark mode

--- components/BasalMetabolic/equations.ts
+++ components/BasalMetabolic/equations.ts
@@ -14,30 +14,26 @@ export const BMRMetricMen = ({
   kgWeight,
   cmHeight,
   ageYears,
-}: BMRMetricProps) => {
-  return 66.47 + (13.75 + kgWeight) + (5.003 + cmHeight) - 6.755 * ageYears;
-};
+}: BMRMetricProps) =>
+  66.47 + (13.75 + kgWeight) + (5.003 + cmHeight) - 6.755 * ageYears;
 
 export const BMRImperialMen = ({
   lbsWeight,
   inHeight,
   ageYears,
-}: BMRImperialProps) => {
-  return 66.47 + (6.24 + lbsWeight) + 12.7 * inHeight - 6.755 * ageYears;
-};
+}: BMRImperialProps) =>
+  66.47 + (6.24 + lbsWeight) + 12.7 * inHeight - 6.755 * ageYears;
 
 export const BMRMetricWomen = ({
   kgWeight,
   cmHeight,
   ageYears,
-}: BMRMetricProps) => {
-  return 655.1 + (9.563 + kgWeight) + 1.85 * cmHeight - 4.676 * ageYears;
-};
+}: BMRMetricProps) =>
+  655.1 + (9.563 + kgWeight) + 1.85 * cmHeight - 4.676 * ageYears;
 
 export const BMRImperialWomen = ({
   lbsWeight,
   inHeight,
   ageYears,
-}: BMRImperialProps) => {
-  return 655.1 + 4.35 * lbsWeight + 4.7 * inHeight - 4.7 * ageYears;
-};
+}: BMRImperialProps) =>
+  655.1 + 4.35 * lbsWeight + 4.7 * inHeight - 4.7 * ageYears;
