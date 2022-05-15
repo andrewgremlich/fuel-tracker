c006f07ed7eb27c80a6a6ee3f0e5083b8bbde04a
Andrew Gremlich <andrew.gremlich@gmail.com>
2022-05-14 21:46:04 -0600

    Connected to API

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -4,10 +4,31 @@ import { IconButton } from "./components/icon-button";
 customElements.define("number-input", NumberInput);
 customElements.define("icon-button", IconButton);
 
-document.getElementById("search-food-button")?.addEventListener("click", () => {
-  const pageNumber = document.getElementById("page-number") as NumberInput;
-  const pageSize = document.getElementById("page-size") as NumberInput;
+document
+  .getElementById("search-food-button")
+  ?.addEventListener("click", async () => {
+    const searchValue = document.getElementById(
+      "search-food-input"
+    ) as HTMLInputElement;
+    const pageNumber = document.getElementById("page-number") as NumberInput;
+    const pageSize = document.getElementById("page-size") as NumberInput;
 
-  console.dir(pageNumber?.number);
-  console.dir(pageSize?.number);
-});
+    if (!searchValue.value) {
+      return;
+    }
+
+    const paramQuerys = {
+      query: searchValue?.value,
+      pageNumber: pageNumber?.number.toString(),
+      pageSize: pageSize?.number.toString(),
+    };
+
+    const foodSearchData = await fetch(
+      `http://localhost:8888/.netlify/functions/search?${new URLSearchParams(
+        paramQuerys
+      ).toString()}`
+    );
+    const foodSearchJson = await foodSearchData.json();
+
+    console.log(foodSearchJson);
+  });
